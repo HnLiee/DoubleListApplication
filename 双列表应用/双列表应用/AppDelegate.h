9b865774f8251a383a0f90e4001a6eb7e7a864e7
HnLiee <1187610615@qq.com>
2016-06-07 11:13:37 +0800

    双列表应用

---
+++ 双列表应用/双列表应用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  双列表应用
+//
+//  Created by HnLiee on 16/6/6.
+//  Copyright © 2016年 HnLiee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 双列表应用/双列表应用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  双列表应用
+//
+//  Created by HnLiee on 16/6/6.
+//  Copyright © 2016年 HnLiee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
